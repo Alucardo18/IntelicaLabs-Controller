b7d8860022c0c257f89d15da7169868380414ad1
Jorge Cano <jacano.sosa@gmail.com>
2019-02-04 15:40:07 -0600

    Add a button to open the about and donate screen

--- app/src/main/java/android/intelica/intelicalabs_controller/controller/fragments/ControllerSettingsFragment.java
+++ app/src/main/java/android/intelica/intelicalabs_controller/controller/fragments/ControllerSettingsFragment.java
@@ -2,6 +2,7 @@ package android.intelica.intelicalabs_controller.controller.fragments;
 
 import android.intelica.intelicalabs_controller.R;
 import android.os.Bundle;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
 public class ControllerSettingsFragment  extends PreferenceFragmentCompat {
@@ -9,5 +10,15 @@ public class ControllerSettingsFragment  extends PreferenceFragmentCompat {
     public void onCreatePreferences(Bundle bundle, String s) {
         // Load the Preferences from the XML file
         addPreferencesFromResource(R.xml.controller_mappings);
+
+        Preference button = findPreference("about_button");
+        button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+
+                //start the about and donate activity
+                return true;
+            }
+        });
     }
 }
