8f381f66dce6186a7c06908938d57cf101fb70fe
Alucardo18 <lauris01>
2019-02-06 17:56:18 -0600

    fancy Buttons dependency

---
+++ app/src/main/java/android/intelica/intelicalabs_controller/controller/PaymentScreen.java
@@ -0,0 +1,14 @@
+package android.intelica.intelicalabs_controller.controller;
+
+import android.intelica.intelicalabs_controller.R;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PaymentScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_screen);
+    }
+}
