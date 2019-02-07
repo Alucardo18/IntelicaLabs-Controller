809f7dc11612718fefabe34a688230b397c24984
Alucardo18 <emmanuel2464@gmail.com>
2019-02-06 22:14:36 -0600

    Right side Buttons

--- app/src/main/java/android/intelica/intelicalabs_controller/controller/PaymentScreen.java
+++ app/src/main/java/android/intelica/intelicalabs_controller/controller/PaymentScreen.java
@@ -1,8 +1,12 @@
 package android.intelica.intelicalabs_controller.controller;
 
+import android.content.pm.ActivityInfo;
 import android.intelica.intelicalabs_controller.R;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class PaymentScreen extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class PaymentScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_screen);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        findViewById(R.id.contactButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(),"has presionado el boton",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
