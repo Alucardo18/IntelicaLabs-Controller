34cd17ae58c4983ea26b367932f7a9f6fec9b6a8
Alucardo18 <lauris01>
2019-02-19 17:22:31 -0600

    Adding Products Ids

--- app/src/main/java/android/intelica/intelicalabs_controller/controller/PaymentScreen.java
+++ app/src/main/java/android/intelica/intelicalabs_controller/controller/PaymentScreen.java
@@ -3,7 +3,7 @@ package android.intelica.intelicalabs_controller.controller;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.intelica.intelicalabs_controller.R;
-import android.intelica.intelicalabs_controller.Util.StaticMessage;
+import android.intelica.intelicalabs_controller.Util.GoogleConsole;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
@@ -15,9 +15,19 @@ import android.widget.Toast;
 import com.anjlab.android.iab.v3.BillingProcessor;
 import com.anjlab.android.iab.v3.TransactionDetails;
 
+import mehdi.sakout.fancybuttons.FancyButton;
+
+import static android.intelica.intelicalabs_controller.Util.GoogleConsole.*;
+
 public class PaymentScreen extends AppCompatActivity implements BillingProcessor.IBillingHandler {
 
-    private BillingProcessor billingProcessor;
+    BillingProcessor billingProcessor;
+    FancyButton billingBtOne;
+    FancyButton billingBtTwo;
+    FancyButton billingBtThree;
+    FancyButton billingBtFour;
+    FancyButton billingBtFive;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +39,14 @@ public class PaymentScreen extends AppCompatActivity implements BillingProcessor
             actionBar.hide();
         }
 
-        billingProcessor = new BillingProcessor(this, "YOUR LICENSE KEY FROM GOOGLE PLAY CONSOLE HERE", this);
+        billingProcessor = new BillingProcessor(this, GoogleConsole.applicationId, this);
         billingProcessor.initialize();
+
+        this.billingBtOne = (FancyButton) findViewById(R.id.firstButton);
+        this.billingBtTwo = (FancyButton) findViewById(R.id.secondButton);
+        this.billingBtThree = (FancyButton) findViewById(R.id.thirdButton);
+        this.billingBtFour = (FancyButton) findViewById(R.id.fourthButton);
+        this.billingBtFive = (FancyButton) findViewById(R.id.fifthButton);
 
         this.paymentAmount();
 
@@ -46,7 +62,9 @@ public class PaymentScreen extends AppCompatActivity implements BillingProcessor
             public void onClick(View view) {
                 // implement Restore purchase method from in-App Billing API 3
                 Toast.makeText(view.getContext(), "has presionado el boton", Toast.LENGTH_SHORT).show();
-                billingProcessor.loadOwnedPurchasesFromGoogle();
+//                billingProcessor.loadOwnedPurchasesFromGoogle();
+                billingProcessor.consumePurchase("android.test.purchased");
+
             }
         });
         findViewById(R.id.shareButton).setOnClickListener(new View.OnClickListener() {
@@ -69,44 +87,41 @@ public class PaymentScreen extends AppCompatActivity implements BillingProcessor
 
 
     public void paymentAmount() {
-        findViewById(R.id.firstButton).setOnClickListener(new View.OnClickListener() {
+        this.billingBtOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//                billingProcessor.purchase(PaymentScreen.this, "android.test.purchased");
+                billingProcessor.purchase(PaymentScreen.this, productOneId);
 
-                v.setEnabled(false);
-                billingProcessor.purchase(PaymentScreen.this,"android.test.purchased");
             }
         });
 
-        findViewById(R.id.secondButton).setOnClickListener(new View.OnClickListener() {
+        this.billingBtTwo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                billingProcessor.purchase(PaymentScreen.this, StaticMessage.PRODUCT_TWO);
-                view.setEnabled(false);
+                billingProcessor.purchase(PaymentScreen.this, productTwoId);
+
             }
         });
 
-        findViewById(R.id.thirdButton).setOnClickListener(new View.OnClickListener() {
+        this.billingBtThree.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                billingProcessor.purchase(PaymentScreen.this, StaticMessage.PRODUCT_THREE);
-                view.setEnabled(false);
+                billingProcessor.purchase(PaymentScreen.this, productThreeId);
             }
         });
 
-        findViewById(R.id.fourthButton).setOnClickListener(new View.OnClickListener() {
+        this.billingBtFour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                billingProcessor.purchase(PaymentScreen.this, StaticMessage.PRODUCT_FOUR);
-                view.setEnabled(false);
+                billingProcessor.purchase(PaymentScreen.this, productFourId);
             }
         });
 
-        findViewById(R.id.fifthButton).setOnClickListener(new View.OnClickListener() {
+        this.billingBtFive.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                billingProcessor.purchase(PaymentScreen.this, StaticMessage.PRODUCT_FIVE);
-                view.setEnabled(false);
+                billingProcessor.purchase(PaymentScreen.this, productFiveId);
             }
         });
 
@@ -118,7 +133,7 @@ public class PaymentScreen extends AppCompatActivity implements BillingProcessor
         String CC = "";
 
         Intent email = new Intent(Intent.ACTION_SEND);
-        email.putExtra(Intent.EXTRA_EMAIL, new String[]{ TO});
+        email.putExtra(Intent.EXTRA_EMAIL, new String[]{TO});
         //email.putExtra(Intent.EXTRA_CC, new String[]{ CC});
         email.putExtra(Intent.EXTRA_SUBJECT, "INSERTA TU TITULO AQUI");
         email.putExtra(Intent.EXTRA_TEXT, "INSERTA TU SUGERENCIA AQUI");
@@ -131,6 +146,29 @@ public class PaymentScreen extends AppCompatActivity implements BillingProcessor
     @Override
     public void onProductPurchased(@NonNull String productId, @Nullable TransactionDetails details) {
         Toast.makeText(this, "GRACIAS POR SU DONACION!", Toast.LENGTH_SHORT).show();
+//        if (billingProcessor.isPurchased(productId)) {
+//            this.billingBtOne.setEnabled(false);
+//        }
+
+        switch (productId) {
+            case GoogleConsole.productOneId :
+                this.billingBtOne.setEnabled(false);
+                break;
+            case GoogleConsole.productTwoId:
+                this.billingBtTwo.setEnabled(false);
+                break;
+            case GoogleConsole.productThreeId:
+                this.billingBtThree.setEnabled(false);
+                break;
+            case GoogleConsole.productFourId:
+                this.billingBtFour.setEnabled(false);
+                break;
+            case GoogleConsole.productFiveId:
+                billingBtFive.setEnabled(false);
+                break;
+            default:
+                Toast.makeText(PaymentScreen.this, "productId not found: " + productId, Toast.LENGTH_LONG).show();
+        }
 
     }
 
@@ -141,7 +179,7 @@ public class PaymentScreen extends AppCompatActivity implements BillingProcessor
 
     @Override
     public void onBillingError(int errorCode, @Nullable Throwable error) {
-
+        Toast.makeText(PaymentScreen.this,"¡SOMETHING WENT WRONG!",Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -163,4 +201,5 @@ public class PaymentScreen extends AppCompatActivity implements BillingProcessor
         }
         super.onDestroy();
     }
+
 }
